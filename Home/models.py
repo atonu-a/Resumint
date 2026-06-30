56ba166c758955c7b7ec4cef1d4508517179b2ca
Atonu <susnigdho1234@gmail.com>
2026-06-30 20:20:11 +0600

    Add Info model with fields for full name, role, email, phone, work experience, and skills; register model in admin

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+class Info(models.Model):
+    full_name = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    email = models.EmailField(max_length=100)
+    phone = PhoneNumberField()
+    work_exp =  models.CharField(max_length=255, blank=True, null=True)
+    work_exp_details = models.TextField(blank=True, null=True)
+    skills = models.TextField(blank=True, null=True)
+    
 
 # Create your models here.
